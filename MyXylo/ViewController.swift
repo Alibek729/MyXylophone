88e437681abdb9302e5e0cdedc40b5eaa5cb0598
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2022-11-23 20:18:27 +0600

    Created Xylophone

--- MyXylo/ViewController.swift
+++ MyXylo/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        if let soundURL = Bundle.main.url(forResource: sender.currentTitle, withExtension: "wav") {
+            player = try? AVAudioPlayer(contentsOf: soundURL)
+            player.play()
+        }
+    }
+    
 }
 
